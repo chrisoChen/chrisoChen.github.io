fdb4681de80345e248b6a7a269b262d31202c819
chrisoChen <chriscarloschen@hotmail.com>
2020-09-07 12:25:24 -0600

    css changes and re-added deliveries form

--- js/page_overlay.js
+++ js/page_overlay.js
@@ -1,43 +1,34 @@
 $(function () {
-  // Open labour overlay when clicking button
+  // Open and close each overlay
   $("#labour_btn").click(function () {
     $("#labour_overlay").css("display", "block");
   });
 
-  // Close labour overlay when clicking "Close" button
   $("#labour_btn_close").click(function () {
     $("#labour_overlay").css("display", "none");
   });
 
-  // Open specs overlay when clicking button
   $("#specs_btn").click(function () {
     $("#specs_overlay").css("display", "block");
   });
 
-  // Close specs overlay when clicking "Close" button
   $("#specs_btn_close").click(function () {
     $("#specs_overlay").css("display", "none");
   });
 
-  // Show selected vehicle on overlay
   $("#specs_select").on("change", function (e) {
     var selectedVehicle = $(this).find("option:selected").attr("value");
 
-    // Hide all current vehicles
     $(".specs_body_grid").css("display", "none");
 
     $("#" + selectedVehicle + "_body_grid").css("display", "grid");
   });
 
-  // Deliveries Overlay: include once implement feature to connect businesses with moovez
-  // 
-  // // Open deliveries overlay when clicking button
-  // $("#deliveries_btn").click(function () {
-  //   $("#deliveries_overlay").css("display", "block");
-  // });
+  $(".deliveries_btn").click(function () {
+    $("#deliveries_overlay").css("display", "block");
+  });
 
-  // // Close deliveries overlay when clicking "Close" button
-  // $("#deliveries_btn_close").click(function () {
-  //   $("#deliveries_overlay").css("display", "none");
-  // });
+  $("#deliveries_btn_close").click(function () {
+    $("#deliveries_overlay").css("display", "none");
+  });
 });
